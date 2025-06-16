a37dc386de199fd91ce132e05f9930883b94ae24
Han-TaeSeong <tjd6487@naver.com>
2025-06-16 13:20:40 +0900

    Update vanilla_diffusion.py

--- models/vanilla_diffusion.py
+++ models/vanilla_diffusion.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.unet import build_dm_unet
+from .unet import build_dm_unet
 from diffusers import DDPMScheduler, DDIMScheduler
 
 
